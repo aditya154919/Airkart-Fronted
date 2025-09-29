c36416220535702ddb0f07ddf3304ecf1fa296d0
user.name = Aditya <Aditya>
2025-09-29 20:24:43 +0530

    Fic router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import "./index.css"; 
 import { ClerkProvider } from '@clerk/clerk-react'
 import { FlightProvider } from "./Context/ApiContext";
@@ -15,11 +15,11 @@ if (!PUBLISHABLE_KEY) {
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
    <FlightProvider>
    <App/>
    </FlightProvider>
    </ClerkProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
